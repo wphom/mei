60d55649ca42a047012ac7a1234d20309d80a6f6
wphom <phom0804@gmail.com>
2024-06-22 07:36:21 +0300

    Add files via upload

--- js files/script2.js
+++ js files/script2.js
@@ -1,5 +1,24 @@
-var leftHand = document.getElementById('left-hand')
-var rightHand = document.getElementById('right-hand')
-var hand2 = leftHand.cloneNode(true)
-hand2.removeAttribute('id')
-rightHand.appendChild(hand2)
+const backHead = document.getElementById('back-head')
+const ear = document.getElementById('ear')
+const mouth = document.getElementById('mouth')
+const nose = document.getElementById('nose')
+const eye = document.getElementById('eye')
+const leftHand = document.getElementById('left')
+const rightHand = document.getElementById('right')
+const text = document.getElementById('text')
+const close = document.getElementById('close')
+
+function close() {
+    text.style.display = 'none'
+}
+
+function open() {
+    if (text.style.display === 'block') {
+        return
+    } else {
+        text.style.display = 'block'
+    }
+}
+backHead.addEventListener('click', () => {
+    text.style.display = 'block'
+})
